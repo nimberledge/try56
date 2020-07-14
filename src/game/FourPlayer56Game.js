844bbad240ce15c88bcb7094b851e732cf7edde7
akhil <akhil2@illinois.edu>
2020-07-14 23:18:06 +0530

    Bidding phase

--- src/game/FourPlayer56Game.js
+++ src/game/FourPlayer56Game.js
@@ -1,6 +1,58 @@
 import * as GameUtils from './GameUtils';
 import { INVALID_MOVE } from 'boardgame.io/core';
 
+function makeBid(G, ctx, amount) {
+	// Make sure bids aren't nonsense
+	if (amount > 56 || amount < 0) {
+		return INVALID_MOVE;
+	}
+	// Make sure first bid is >= 28
+	if (G.bids.length === 0) {
+		if (amount < 28) {
+			return INVALID_MOVE;
+		}
+	} else {
+		// Make sure that you can only outbid or pass
+		if (amount <= G.game_bid.bid_value && amount !== 0) {
+			return INVALID_MOVE;
+		}
+	}
+	// If you're outbidding a teammate in the 28-39 range
+	// you must bid 40+
+	if (G.game_bid != null && G.game_bid.bid_value < 40 && amount < 40 && amount !== 0) {
+		if (G.players[G.game_bid.player].team === G.players[ctx.currentPlayer].team) {
+			return INVALID_MOVE;
+		}
+	}
+	// If you've outbid the current bid on table, congrats
+	if (G.game_bid == null || amount > G.game_bid.bid_value) {
+		G.game_bid = { player: ctx.currentPlayer, bid_value: amount };
+	}
+
+	// Log the history
+	G.bids.push({ player: ctx.currentPlayer, bid_value: amount });
+}
+
+function isBiddingDone(G, ctx) {
+	// If the current bid can't be outbid we're done
+	if (G.game_bid != null && G.game_bid.bid_value === 56) {
+		return true;
+	}
+	// If there's < 4 bids bidding can't be done
+	// Else bidding is done when there are 3 passes in a row
+	if (G.bids.length >= 4) {
+		var bid_sum = 0;
+		for (let i = G.bids.length - 3; i < G.bids.length; i++) {
+			bid_sum += G.bids[i].bid_value;
+		}
+		console.log(bid_sum);
+		if (bid_sum === 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
 export const FourPlayer56Game = {
 	name: "Four-Player-56",
 	setup: () => {
@@ -31,37 +83,26 @@ export const FourPlayer56Game = {
 	turn: {
 		moveLimit: 1,
 	},
-	moves: {
-		makeBid: {
-				move: (G, ctx, amount) => {
-					// Make sure bids aren't nonsense
-					if (amount > 56 || amount < 0) {
-						return INVALID_MOVE;
-					}
-					// Make sure first bid is >= 28
-					if (G.bids.length === 0) {
-						if (amount < 28) {
-							return INVALID_MOVE;
-						}
-					} else {
-						// Make sure that you can only outbid or pass
-						if (amount <= G.game_bid.bid_value && amount !== 0) {
-							return INVALID_MOVE;
-						}
-					}
-					if (G.game_bid == null || amount > G.game_bid.bid_value) {
-						G.game_bid = { player: ctx.currentPlayer, bid_value: amount };
-					}
-					G.bids.push({ player: ctx.currentPlayer, bid_value: amount });
-			}
-		},
 
-		selectHideTrump: {
-			move: (G, ctx) => {}
+	phases: {
+		bid_phase: {
+			moves: { makeBid },
+			start: true,
+			next: null,
+			endIf: isBiddingDone,
 		},
+		// hide_trump_phase: {
+		// 	moves: { hideTrump }
+		// },
+	},
 
-		playCardFromHand: {
-			move: (G, ctx) => {}
-		}
-	}
+	// moves: {
+	// 	selectHideTrump: {
+	// 		move: (G, ctx) => {}
+	// 	},
+	//
+	// 	playCardFromHand: {
+	// 		move: (G, ctx) => {}
+	// 	}
+	// }
 };
