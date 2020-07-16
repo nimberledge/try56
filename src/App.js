893f2a065f60c6fcc1d550e6573d4b23f9ad59b7
akhil <akhil2@illinois.edu>
2020-07-17 01:55:56 +0530

    Shell script commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Client } from 'boardgame.io/react';
 import { FourPlayer56Game } from './game/FourPlayer56Game';
+import { FourPlayerGameTable } from './components/FourPlayerGameTable';
 
-const App = Client({ game: FourPlayer56Game, numPlayers: 4 });
+const App = Client({ game: FourPlayer56Game, board: FourPlayerGameTable, numPlayers: 4 });
 
 export default App;
--- src/components/FourPlayerGameTable.js
+++ src/components/FourPlayerGameTable.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+// var cards = require('cardsJS');
 
 export class FourPlayerGameTable extends React.Component {
-
-}
+	render() {
+		// return <img class='card' src='./cardsJS/cards/AS.svg' alt='AS'></img>;
+	}
+};
