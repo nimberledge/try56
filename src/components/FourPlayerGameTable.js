4bb43f994fbaf6673e0c48fbe3cfa8ce44ff94e7
akhil <akhil2@illinois.edu>
2020-07-17 22:41:49 +0530

    Shell script commit

--- src/components/FourPlayerGameTable.js
+++ src/components/FourPlayerGameTable.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
+function generateCard(card) {
+	let retval = document.createElement('img');
+	retval.className = 'card';
+	retval.src = 'cards/' + card.rank + card.suit + '.svg';
+	return retval;
+}
+
 export class FourPlayerGameTable extends React.Component {
 	render() {
-		return <div class="hand hhand-compact active-hand">
-    <img class='card' src='cards/AS.svg'></img>
-    <img class='card' src='cards/KS.svg'></img>
-    <img class='card' src='cards/QS.svg'></img>
-    <img class='card' src='cards/JS.svg'></img>
-    <img class='card' src='cards/TS.svg'></img>
-    <img class='card' src='cards/9H.svg'></img>
-    <img class='card' src='cards/3H.svg'></img>
-		</div>;
+		let phase = <div id="Current phase">Current game phase: {this.props.ctx.phase}</div>;
+		let current_player_id = this.props.ctx.currentPlayer;
+		let current_player = <div id="Current player">Current player: { current_player_id }</div>;
+		let player_cards = [];
+		let alt_card_str = '';
+		let cards = []
+		for (let i = 0; i < this.props.G.players[current_player_id].cards.length; i++) {
+			var card = this.props.G.players[current_player_id].cards[i];
+			let card_src = 'cards/' + card.rank + card.suit + '.svg'
+			cards.push(<img class='card' src={ card_src } />);
+		}
+		let hand_div = <div class='hand hhand-compact active-hand'>
+			{ cards }
+		</div>
+		// let alt_cards = <div id="Cards-text"> Cards: { alt_card_str }</div>;
+		// let data_hand_str = "cards: " + player_cards;
+		// let player_hand = <div className="hand hhand-compact active-hand"
+		// 		data-hand={ data_hand_str } />;
+		return <div>
+		{ phase }
+		{ current_player }
+		{ hand_div }
+		</div>
 	}
 };
