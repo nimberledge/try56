b5fb37e109982776b2957eb33671e18b31954a0b
akhil <akhil2@illinois.edu>
2020-07-17 13:04:53 +0530

    Shell script commit

--- src/components/FourPlayerGameTable.js
+++ src/components/FourPlayerGameTable.js
@@ -1,8 +1,15 @@
 import React from 'react';
-// var cards = require('cardsJS');
 
 export class FourPlayerGameTable extends React.Component {
 	render() {
-		// return <img class='card' src='./cardsJS/cards/AS.svg' alt='AS'></img>;
+		return <div class="hand hhand-compact active-hand">
+    <img class='card' src='cards/AS.svg'></img>
+    <img class='card' src='cards/KS.svg'></img>
+    <img class='card' src='cards/QS.svg'></img>
+    <img class='card' src='cards/JS.svg'></img>
+    <img class='card' src='cards/TS.svg'></img>
+    <img class='card' src='cards/9H.svg'></img>
+    <img class='card' src='cards/3H.svg'></img>
+		</div>;
 	}
 };
