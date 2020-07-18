604e64adee03ae3a6232f20e9c05eb4d6de7597c
akhil <akhil2@illinois.edu>
2020-07-18 13:16:43 +0530

    a green rectangle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Client } from 'boardgame.io/react';
 import { FourPlayer56Game } from './game/FourPlayer56Game';
-import { DummyDebugTable } from './components/DummyDebugTable';
+// import { DummyDebugTable } from './components/DummyDebugTable';
+import { FourPlayerGameTable } from './components/FourPlayerGameTable';
 
-const App = Client({ game: FourPlayer56Game, board: DummyDebugTable, numPlayers: 4 });
+const App = Client({ game: FourPlayer56Game, board: FourPlayerGameTable, numPlayers: 4 });
 
 export default App;
--- src/components/FourPlayerGameTable.jsx
+++ src/components/FourPlayerGameTable.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
 
 export class FourPlayerGameTable extends React.Component {
+	render() {
+		let table_div = <div className='table' id='game_table' />;
+		return table_div;
+	}
 };
