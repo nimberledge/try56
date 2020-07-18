65539692db4cda4fc3da88fdb169cc451a01e8bf
akhil <akhil2@illinois.edu>
2020-07-18 17:32:25 +0530

    kms

--- src/components/FourPlayerGameTable.jsx
+++ src/components/FourPlayerGameTable.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import PlayerCards from './PlayerCards'
+import PlayerCards from './PlayerCards';
+import TeammateCards from './TeammateCards';
+import OpponentCards from './OpponentCards';
 
 export class FourPlayerGameTable extends React.Component {
 	render() {
 		let current_player_cards = this.props.G.players[this.props.ctx.currentPlayer].cards;
+		let teammate_cards = this.props.G.players[(this.props.ctx.currentPlayer + 2) % 4].cards;
+		let opp_left_cards = this.props.G.players[(this.props.ctx.currentPlayer + 1) % 4].cards;
+		let opp_right_cards = this.props.G.players[(this.props.ctx.currentPlayer + 3) % 4].cards;
 		return <div className='GameTable' id='game_table'>
 			<div id='player_cards_container'>
 				<PlayerCards id="player_cards" cards= { current_player_cards } />
+			</div>
+			<div id='opp_left_cards_container'>
+				<OpponentCards id="opp_left_cards" cards= { opp_left_cards } />
+			</div>
+			<div id='teammate_cards_container'>
+				<TeammateCards id="teammate_cards" cards= { teammate_cards } />
+			</div>
+			<div id='opp_right_cards_container'>
+				<OpponentCards id="opp_right_cards" cards= { opp_right_cards } />
 			</div>
 		</div>;
 	}
---
+++ src/components/OpponentCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class OpponentCards extends React.Component {
+	render () {
+		let card_imgs = [];
+		for (let i = 0; i < this.props.cards.length; i++) {
+			card_imgs.push(<img class='card' src='cards/Blue_Back.svg' alt='rip' />);
+		}
+		return <div className="hand hhand-compact"> { card_imgs } </div>;
+	}
+}
+
+export default OpponentCards;
